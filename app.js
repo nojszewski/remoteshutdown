51f93cff0801bbdbcb4926b582b1c92762335b2b
Wojciech Nojszewski <wojciech@nojszewski.net>
2025-03-26 13:26:43 +0100

    fix

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const domain = '.zs1mm.local'
 
 app.get('/shutdown/:computerName', (req, res) => {
     const computerName = req.params.computerName + domain
-    const cmd = `powershell.exe -Command \"& \"C:\\Program Files\\Veyon\\veyon-cli.exe\" feature start ${computerName} PowerDownNow\"`
+    const cmd = `C:\Program Files\Veyon\veyon-cli.exe feature start ${computerName} PowerDownNow`
 
     exec(cmd, (error, stdout, stderr) => {
         if (error) {
@@ -25,6 +25,46 @@ app.get('/shutdown/:computerName', (req, res) => {
 
         console.log(`stdout: ${stdout}`)
         res.send(`Komputer ${computerName} został wyłączony`)
+    })
+})
+
+app.get('/lock/:computerName', (req, res) => {
+    const computerName = req.params.computerName + domain
+    const cmd = `C:\Program Files\Veyon\veyon-cli.exe feature start ${computerName} ScreenLock`
+
+    exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error: ${error.message}`)
+            return res.status(500).send(`Error: ${error.message}`)
+        }
+
+        if (stderr) {
+            console.error(`stderr: ${stderr}`)
+            return res.status(500).send(`stderr: ${stderr}`)
+        }
+
+        console.log(`stdout: ${stdout}`)
+        res.send(`Zablokowano komputer ${computerName}`)
+    })
+})
+
+app.get('/unlock/:computerName', (req, res) => {
+    const computerName = req.params.computerName + domain
+    const cmd = `C:\Program Files\Veyon\veyon-cli.exe feature stop ${computerName} ScreenLock`
+
+    exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error: ${error.message}`)
+            return res.status(500).send(`Error: ${error.message}`)
+        }
+
+        if (stderr) {
+            console.error(`stderr: ${stderr}`)
+            return res.status(500).send(`stderr: ${stderr}`)
+        }
+
+        console.log(`stdout: ${stdout}`)
+        res.send(`Odblokowano komputer ${computerName}`)
     })
 })
 
