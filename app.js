4f5c0b781c3739b5daeb558533ef4f2e08368a47
Wojciech Nojszewski <wojciech@nojszewski.net>
2025-03-26 18:12:55 +0100

    add option to lock multiple pcs

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const domain = '.zs1mm.local'
 
 app.get('/shutdown/:computerName', (req, res) => {
     const computerName = req.params.computerName + domain
-    const cmd = `C:\Program Files\Veyon\veyon-cli.exe feature start ${computerName} PowerDownNow`
+    const cmd = `"C:\\Program Files\\Veyon\\veyon-cli.exe" feature start ${computerName} PowerDownNow`
 
     exec(cmd, (error, stdout, stderr) => {
         if (error) {
@@ -29,28 +29,66 @@ app.get('/shutdown/:computerName', (req, res) => {
 })
 
 app.get('/lock/:computerName', (req, res) => {
-    const computerName = req.params.computerName + domain
-    const cmd = `C:\Program Files\Veyon\veyon-cli.exe feature start ${computerName} ScreenLock`
+    if (req.params.computerName === '212') {
+        const computers = [
+            'K-212-01',
+            'K-212-02',
+            'K-212-03',
+            'K-212-04',
+            'K-212-05',
+            'K-212-06',
+            'K-212-07',
+            'K-212-08',
+            'K-212-09',
+            'K-212-10',
+            'K-212-11',
+            'K-212-12',
+            'K-212-13',
+            'K-212-14'
+        ]
 
-    exec(cmd, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error: ${error.message}`)
-            return res.status(500).send(`Error: ${error.message}`)
-        }
+        computers.forEach(computer => {
+            const cmd = `"C:\\Program Files\\Veyon\\veyon-cli.exe" feature start ${computer + domain} ScreenLock`
+            exec(cmd, (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`Error: ${error.message}`)
+                    return res.status(500).send(`Error: ${error.message}`)
+                }
+    
+                if (stderr) {
+                    console.error(`stderr: ${stderr}`)
+                    return res.status(500).send(`stderr: ${stderr}`)
+                }
+    
+                console.log(`stdout: ${stdout}`)
+                res.send(`Komputer ${computer} został zablokowany`)
+            })
+        })
 
-        if (stderr) {
-            console.error(`stderr: ${stderr}`)
-            return res.status(500).send(`stderr: ${stderr}`)
-        }
+    } else {
+        computerName = req.params.computerName + domain
+        const computerName = req.params.computerName + domain
+        const cmd = `"C:\\Program Files\\Veyon\\veyon-cli.exe" feature start ${computerName} ScreenLock`
+        exec(cmd, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error: ${error.message}`)
+                return res.status(500).send(`Error: ${error.message}`)
+            }
 
-        console.log(`stdout: ${stdout}`)
-        res.send(`Zablokowano komputer ${computerName}`)
-    })
+            if (stderr) {
+                console.error(`stderr: ${stderr}`)
+                return res.status(500).send(`stderr: ${stderr}`)
+            }
+
+            console.log(`stdout: ${stdout}`)
+            res.send(`Zablokowano komputer ${computerName}`)
+        })
+    }
 })
 
 app.get('/unlock/:computerName', (req, res) => {
     const computerName = req.params.computerName + domain
-    const cmd = `C:\Program Files\Veyon\veyon-cli.exe feature stop ${computerName} ScreenLock`
+    const cmd = `"C:\\Program Files\\Veyon\\veyon-cli.exe" feature stop ${computerName} ScreenLock`
 
     exec(cmd, (error, stdout, stderr) => {
         if (error) {
