bca80a1de895abd40cc7a6639ff2124826dd4b51
Wojciech Nojszewski <wojciech@nojszewski.net>
2025-04-23 23:18:23 +0200

    update

--- app.js
+++ app.js
@@ -2,102 +2,124 @@ const express = require('express')
 const app = express()
 const { exec } = require('child_process')
 const cors = require('cors')
+const fs = require('fs')
 
 app.use(cors())
-const domain = '.zs1mm.local'
 
-const computers = [];
-for (let i = 1; i <= 14; i++) {
-    computers.push(`K-212-${String(i).padStart(2, '0')}`)
+try {
+    // zobacz config.json
+    const config = JSON.parse(fs.readFileSync('./config.json', 'utf8'))
+    if (Array.isArray(config.computers)) {
+        computers = config.computers
+    } else {
+        console.error('Invalid config file: "computers" should be an array.')
+    }
+} catch (error) {
+    console.error('Error reading or parsing config.json:', error.message)
 }
 
+// endpoint do wyłączania kompów
 app.get('/shutdown/:computerName', (req, res) => {
     if (req.params.computerName === 'all') {
         let completed = 0
         
         computers.forEach(computer => {
-            const cmd = `"C:\\Program Files\\Veyon\\veyon-cli.exe" feature start ${computer + domain} PowerDownNow`
+            const cmd = `"C:\\Program Files\\Veyon\\veyon-cli.exe" feature start ${computer} PowerDownNow`
             exec(cmd, (error, stdout, stderr) => {
                 if (error || stderr) {
                     console.log(`Błąd przy ${computer}: ${error ? error.message : stderr}`)
+                    if (!res.headersSent) {
+                        res.send('failed')
+                    }
+                    return
                 }
                 completed++
                 if (completed === computers.length) {
-                    res.send('Wszystkie komputery zostały wyłączone')
+                    res.send('success')
                 }
             })
         })
     } else {
-        const computerName = req.params.computerName + domain
+        const computerName = req.params.computerName
         const cmd = `"C:\\Program Files\\Veyon\\veyon-cli.exe" feature start ${computerName} PowerDownNow`
         exec(cmd, (error, stdout, stderr) => {
             if (error || stderr) {
                 console.log(`Błąd przy ${computerName}: ${error ? error.message : stderr}`)
                 return res.send('failed')
             }
-            res.send(`Wyłączono komputer ${computerName}`)
+            res.send('success')
         })
     }
 })
 
+// endpoint do blokowania kompów
 app.get('/lock/:computerName', (req, res) => {
     if (req.params.computerName === 'all') {
         let completed = 0
         
         computers.forEach(computer => {
-            const cmd = `"C:\\Program Files\\Veyon\\veyon-cli.exe" feature start ${computer + domain} ScreenLock`
+            const cmd = `"C:\\Program Files\\Veyon\\veyon-cli.exe" feature start ${computer} ScreenLock`
             exec(cmd, (error, stdout, stderr) => {
                 if (error || stderr) {
                     console.log(`Błąd przy ${computer}: ${error ? error.message : stderr}`)
+                    if (!res.headersSent) {
+                        res.send('failed')
+                    }
+                    return
                 }
                 completed++
                 if (completed === computers.length) {
-                    res.send('Wszystkie komputery zostały zablokowane')
+                    res.send('success')
                 }
             })
         })
     } else {
-        const computerName = req.params.computerName + domain
+        const computerName = req.params.computerName
         const cmd = `"C:\\Program Files\\Veyon\\veyon-cli.exe" feature start ${computerName} ScreenLock`
         exec(cmd, (error, stdout, stderr) => {
             if (error || stderr) {
                 console.log(`Błąd przy ${computerName}: ${error ? error.message : stderr}`)
                 return res.send('failed')
             }
-            res.send(`Zablokowano komputer ${computerName}`)
+            res.send('success')
         })
     }
 })
 
+// endpoint do odblokowywania kompów
 app.get('/unlock/:computerName', (req, res) => {
     if (req.params.computerName === 'all') {
         let completed = 0
         
         computers.forEach(computer => {
-            const cmd = `"C:\\Program Files\\Veyon\\veyon-cli.exe" feature stop ${computer + domain} ScreenLock`
+            const cmd = `"C:\\Program Files\\Veyon\\veyon-cli.exe" feature stop ${computer} ScreenLock`
             exec(cmd, (error, stdout, stderr) => {
                 if (error || stderr) {
                     console.log(`Błąd przy ${computer}: ${error ? error.message : stderr}`)
+                    if (!res.headersSent) {
+                        res.send('failed')
+                    }
+                    return
                 }
                 completed++
                 if (completed === computers.length) {
-                    res.send('Wszystkie komputery zostały odblokowane')
+                    res.send('success')
                 }
             })
         })
     } else {
-        const computerName = req.params.computerName + domain
-        const cmd = `"C:\\Program Files\\Veyon\\veyon-cli.exe" feature stop ${computerName} ScreenLock`
+        const computerName = req.params.computerName
+        const cmd = `"C:\\Program Files\\Veyon\\veyon-cli.exe" feature start ${computerName} ScreenLock`
         exec(cmd, (error, stdout, stderr) => {
             if (error || stderr) {
                 console.log(`Błąd przy ${computerName}: ${error ? error.message : stderr}`)
                 return res.send('failed')
             }
-            res.send(`Odblokowano komputer ${computerName}`)
+            res.send('success')
         })
     }
 })
 
 app.listen(3000, () => {
-    console.log('OK')
+    console.log('OK, działa na porcie 3000')
 })
